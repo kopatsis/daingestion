2d817f6fbb47227e295c6a0143d9c1b130281141
Demetrios Kopatsis <jkopatsis@gmail.com>
2025-12-05 15:00:24 -0800

    basis for go ingestion

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module dmd
 
-go 1.23.1
+go 1.24.0
+
+toolchain go1.24.11
+
+require (
+	github.com/oschwald/maxminddb-golang/v2 v2.1.1 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,67 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"os"
 
+	"github.com/oschwald/maxminddb-golang/v2"
+)
+
+type DataCenterASNs struct {
+	set map[uint]struct{}
+}
+
+var (
+	asnList DataCenterASNs
+	geoDB   *maxminddb.Reader
+)
+
+func LoadASNs(path string) (DataCenterASNs, error) {
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return DataCenterASNs{}, err
+	}
+	s := DataCenterASNs{set: map[uint]struct{}{}}
+	start := 0
+	for i := 0; i <= len(b); i++ {
+		if i == len(b) || b[i] == '\n' {
+			if i > start {
+				var n uint
+				parseNum(b[start:i], &n)
+				s.set[n] = struct{}{}
+			}
+			start = i + 1
+		}
+	}
+	return s, nil
+}
+
+func parseNum(b []byte, out *uint) {
+	var n uint
+	for _, c := range b {
+		if c >= '0' && c <= '9' {
+			n = n*10 + uint(c-'0')
+		}
+	}
+	*out = n
+}
+
+func main() {
+	a, err := LoadASNs("files/asn.txt")
+	if err != nil {
+		panic(err)
+	}
+	asnList = a
+
+	db, err := maxminddb.Open("files/GeoLite2-City.mmdb")
+	if err != nil {
+		panic(err)
+	}
+	geoDB = db
+
+	http.HandleFunc("/ingest", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
