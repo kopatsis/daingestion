2eae2e10265fe0afa9e598b981e3a99a074cc2e8
Demetrios Kopatsis <jkopatsis@gmail.com>
2025-12-09 17:25:59 -0800

    metadata system for tracking

---
+++ later/livecount.go
@@ -0,0 +1,49 @@
+package later
+
+import (
+	"context"
+	"dmd/steps"
+	"strconv"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func GetSessionField(ctx context.Context, rdb *redis.Client, sessionID, field string) (string, error) {
+	key := "session:" + sessionID
+	v, err := rdb.HGet(ctx, key, field).Result()
+	if err == redis.Nil {
+		return "", nil
+	}
+	return v, err
+}
+
+func GetSessionAll(ctx context.Context, rdb *redis.Client, sessionID string) (steps.GeoData, error) {
+	key := "session:" + sessionID
+	m, err := rdb.HGetAll(ctx, key).Result()
+	if err == redis.Nil {
+		return steps.GeoData{}, nil
+	}
+	if err != nil {
+		return steps.GeoData{}, err
+	}
+
+	lat, _ := strconv.ParseFloat(m["Latitude"], 64)
+	lon, _ := strconv.ParseFloat(m["Longitude"], 64)
+	acc, _ := strconv.ParseUint(m["AccuracyRadius"], 10, 16)
+	asn, _ := strconv.ParseUint(m["ASN"], 10, 32)
+
+	return steps.GeoData{
+		IP:              m["IP"],
+		CountryISO:      m["CountryISO"],
+		CountryName:     m["CountryName"],
+		SubdivisionISO:  m["SubdivisionISO"],
+		SubdivisionName: m["SubdivisionName"],
+		CityName:        m["CityName"],
+		PostalCode:      m["PostalCode"],
+		Latitude:        lat,
+		Longitude:       lon,
+		AccuracyRadius:  uint16(acc),
+		ASN:             uint(asn),
+		ASNOrg:          m["ASNOrg"],
+	}, nil
+}
--- live/count.go
+++ live/count.go
@@ -2,7 +2,9 @@ package live
 
 import (
 	"context"
+	"dmd/steps"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -10,8 +12,30 @@ import (
 
 func AddActive(rdb *redis.Client, tenant, dim, id, sessionID string) {
 	now := time.Now()
-	b := now.Unix() / 10
+	b := now.Unix() / 15
 	k := fmt.Sprintf("active:%s:%s:%s:%d", tenant, dim, id, b)
 	rdb.SAdd(context.TODO(), k, sessionID)
-	rdb.Expire(context.TODO(), k, 4*time.Minute)
+	rdb.Expire(context.TODO(), k, 4*time.Minute+15*time.Second)
+}
+
+func SetSessionMetadata(ctx context.Context, rdb *redis.Client, sessionID string, g steps.GeoData) error {
+	key := "session:" + sessionID
+	_, err := rdb.HSet(ctx, key, map[string]interface{}{
+		"IP":              g.IP,
+		"CountryISO":      g.CountryISO,
+		"CountryName":     g.CountryName,
+		"SubdivisionISO":  g.SubdivisionISO,
+		"SubdivisionName": g.SubdivisionName,
+		"CityName":        g.CityName,
+		"PostalCode":      g.PostalCode,
+		"Latitude":        strconv.FormatFloat(g.Latitude, 'f', -1, 64),
+		"Longitude":       strconv.FormatFloat(g.Longitude, 'f', -1, 64),
+		"AccuracyRadius":  strconv.Itoa(int(g.AccuracyRadius)),
+		"ASN":             strconv.Itoa(int(g.ASN)),
+		"ASNOrg":          g.ASNOrg,
+	}).Result()
+	if err != nil {
+		return err
+	}
+	return rdb.Expire(ctx, key, 300000000000).Err()
 }
